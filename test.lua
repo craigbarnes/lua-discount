1173fa57b6893f621607db7b992c84146068a055
Craig Barnes <cr@igbarn.es>
2013-10-04 14:33:10 +0100

    Fix test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,5 @@
 package.path  = ''
 package.cpath = './?.so'
-
-local markdown = require "ldiscount"
+local markdown = require "discount"
 
 assert(markdown "*It works!*" == "<p><em>It works!</em></p>")
