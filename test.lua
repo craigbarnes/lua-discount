ff28ddcd62c8c814fbabd146dbe49c256ddc6288
Craig Barnes <cr@igbarn.es>
2013-10-12 02:12:39 +0100

    Add tests

--- test.lua
+++ test.lua
@@ -1,5 +1,12 @@
 package.path  = ''
 package.cpath = './?.so'
 local markdown = require "discount"
+local doc, toc
 
 assert(markdown "*It works!*" == "<p><em>It works!</em></p>")
+
+doc, toc = markdown("", "toc")
+assert(doc == "" and toc == "")
+
+doc, toc = markdown("")
+assert(doc == "" and not toc)
